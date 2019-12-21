58a1040a96b60144e7593d34fc178d5aed82fb5b
坂本将紀 <masaki19821208@gmail.com>
2019-12-21 17:38:56 +0900

    一旦、マルバツ画面に遷移するようにします。ジャンケン画面ができたらそっちに遷移処理を変更します。

--- MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
+++ MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, JyankenActivity.class));
+                startActivity(new Intent(MainActivity.this, MaruBatuActivity.class));
                 finish();
             }
         });
