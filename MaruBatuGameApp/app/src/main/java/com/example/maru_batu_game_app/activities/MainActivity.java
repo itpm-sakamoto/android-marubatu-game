ff1f39747105feaa9a758db4f5906a7aca0f6eba
坂本将紀 <masaki19821208@gmail.com>
2019-12-21 17:34:40 +0900

    TOP画面を作成しました。

--- MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
+++ MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.maru_batu_game_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.maru_batu_game_app.R;
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button startBtn = findViewById(R.id.start_btn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, JyankenActivity.class));
+                finish();
+            }
+        });
     }
 }
