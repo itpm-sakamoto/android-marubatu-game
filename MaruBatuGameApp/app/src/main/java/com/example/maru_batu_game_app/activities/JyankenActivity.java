5ae9e7bbe964e849683e77629b6e5fcd3cea927e
坂本将紀 <masaki19821208@gmail.com>
2019-12-21 12:57:10 +0900

    各Activityを用意しました。

---
+++ MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/JyankenActivity.java
@@ -0,0 +1,16 @@
+package com.example.maru_batu_game_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maru_batu_game_app.R;
+
+public class JyankenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jyanken);
+    }
+}
--- MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
+++ MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.maru_batu_game_app;
+package com.example.maru_batu_game_app.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.maru_batu_game_app.R;
 
 public class MainActivity extends AppCompatActivity {
 
