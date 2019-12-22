0029f7c9c1eb46135fd9205c0733ca5d39d35592
坂本将紀 <masaki19821208@gmail.com>
2019-12-22 16:48:18 +0900

    スプラッシュデザイン変更。

--- MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
+++ MaruBatuGameApp/app/src/main/java/com/example/maru_batu_game_app/activities/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.maru_batu_game_app.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.maru_batu_game_app.BuildConfig;
 import com.example.maru_batu_game_app.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d("TAG", getString(R.string.version_name, BuildConfig.VERSION_NAME));
         Button startBtn = findViewById(R.id.start_btn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
